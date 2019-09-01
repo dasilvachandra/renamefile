4fae834fb4ad1d7f3f3b71374127578b283df633
dasilvachandra <chandrad176@gmail.com>
2019-09-01 14:27:58 +0700

    menambahkan parameter untuk di depan nama file

--- renamefile.sh
+++ renamefile.sh
@@ -1,16 +1,23 @@
 a=1;
 b=$(ls | wc -l);
+firstname=$1;
 mkdir .temp
+if [[ -z "$firstname" ]]; then
+    firstname="";
+fi
 while (("$a" <= "$b" )); 
     do 
+        
         format=$(ls | tail -n $a | head -n 1| grep -Eoi [.]+.+);
         from="$(ls | sort | tail -n $a | head -n 1 )";
         to=$(ls -lt --time-style="+%Y-%m-%d_%H%M%S($a)"| grep "$from" | tail -n $a | head -n 1 | awk '{print $6}');
-        if [[ -f $to$format ]]; then
-            echo "$to$format exist"
+        namefile=$firstname$to$format;
+        if [[ -f $namefile ]]; then
+            echo "$namefile exist"
+            echo "rename file '$from' = $namefile";
         else 
-            mv "$from" .temp/$to$format;
-            echo "rename file '$from' = $to$format";
+            mv "$from" .temp/$namefile;
+            echo "rename file '$from' = $namefile";
         fi
         a=$[$a+1];
     done;
