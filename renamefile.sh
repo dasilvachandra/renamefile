2da9aa4b4f7aa5d4cc7a7f39b148e016f3827a5b
dasilvachandra <chandrad176@gmail.com>
2019-09-01 13:57:10 +0700

    membuat rename file

---
+++ renamefile.sh
@@ -0,0 +1,17 @@
+a=1;
+b=$(ls | wc -l);
+mkdir .temp
+while (("$a" <= "$b" )); 
+    do 
+        format=$(ls | tail -n $a | head -n 1| grep -Eoi [.]+.+);
+        # mv *.$format .temp;
+        from="$(ls | sort | tail -n $a | head -n 1 )";
+        to=$(ls -lt --time-style="+%Y-%m-%d_%H%M%S($a)"| grep "$from" | tail -n $a | head -n 1 | awk '{print $6}');
+        if [[ -f $to$format ]]; then
+            echo "$to$format exist"
+        else 
+            mv "$from" .temp/$to$format;
+            echo "rename file '$from' = $to$format";
+        fi
+        a=$[$a+1];
+    done;
